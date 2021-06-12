4de293bd5808d5c63ac02c0db4a1af22061651b3
Luka Maisuradze <lmaisuradz3@gmail.com>
2021-06-12 22:20:37 +0400

    Configure cookie-session in auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import cookieSession from "cookie-session";
 
 import mongoose from "mongoose";
 
@@ -13,8 +14,15 @@ import { errorHandler } from "./middlewares/error-hander";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 
 app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: true,
+	}),
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
