512f23f7ee1977908735c2c41511af35e098932e
Luka Maisuradze <lmaisuradz3@gmail.com>
2021-06-21 15:51:16 +0400

    Simulate sign in in tickets service for testing

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -3,10 +3,12 @@ import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../app";
 
+import jwt from "jsonwebtoken";
+
 declare global {
 	namespace NodeJS {
 		interface Global {
-			signin(): Promise<string[]>;
+			signin(): string[];
 		}
 	}
 }
@@ -37,13 +39,19 @@ afterAll(async () => {
 });
 
 // This function could be a helper function not a global one
-global.signin = async () => {
-	const email = "test@example.com";
-	const password = "password";
+global.signin = () => {
+	const payload = {
+		id: "randomId12345",
+		email: "test@example.com",
+	};
 
-	const res = await request(app).post("/api/users/signup").send({ email, password }).expect(201);
+	const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-	const cookie = res.get("Set-Cookie");
+	const session = { jwt: token };
 
-	return cookie;
+	const sessionJSON = JSON.stringify(session);
+
+	const base64 = Buffer.from(sessionJSON).toString("base64");
+
+	return [`express:sess=${base64}`];
 };
