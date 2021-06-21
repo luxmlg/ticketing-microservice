f3f803d8a163b199192464b97b49580439306893
Luka Maisuradze <lmaisuradz3@gmail.com>
2021-06-21 18:35:34 +0400

    Make some formatting changes

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
-import { Ticket } from "../models/ticket";
-
 import { requireAuth, validateRequest } from "@luxticketing/common";
+import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
@@ -10,14 +9,18 @@ router.post(
 	"/api/tickets",
 	requireAuth,
 	[
-		body("title").notEmpty().withMessage("Title is required"),
+		body("title").not().isEmpty().withMessage("Title is required"),
 		body("price").isFloat({ gt: 0 }).withMessage("Price must be greater than 0"),
 	],
 	validateRequest,
 	async (req: Request, res: Response) => {
 		const { title, price } = req.body;
 
-		const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
+		const ticket = Ticket.build({
+			title,
+			price,
+			userId: req.currentUser!.id,
+		});
 		await ticket.save();
 
 		res.status(201).send(ticket);
