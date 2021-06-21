8c4eb6e051f717e3eee9cfa7767f7bb85bfca781
Luka Maisuradze <lmaisuradz3@gmail.com>
2021-06-21 17:46:45 +0400

    Implement update router and add it to app

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -8,6 +8,7 @@ import { errorHandler, NotFoundError, currentUser } from "@luxticketing/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
 import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -24,6 +25,7 @@ app.use(currentUser);
 app.use(indexTicketRouter);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(updateTicketRouter);
 
 // Example async route
 app.all("*", async () => {
