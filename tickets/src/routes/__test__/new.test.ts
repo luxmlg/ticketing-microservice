0ea6a67f46aa921e6545057c14fa504ec5c9d535
Luka Maisuradze <lmaisuradz3@gmail.com>
2021-06-24 13:02:58 +0400

    Check if events publish successfully

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("has a router handler listening to /api/tickets for post requests", async () => {
 	const res = await request(app).post("/api/tickets").send({});
@@ -75,4 +76,17 @@ it("creates a ticket with valid inputs", async () => {
 	expect(tickets.length).toEqual(1);
 	expect(tickets[0].title).toEqual(title);
 	expect(tickets[0].price).toEqual(price);
+});
+
+it("publishes an event", async () => {
+	const title = "SOME TITLE";
+	const price = 10;
+
+	await request(app)
+		.post("/api/tickets")
+		.set("Cookie", global.signin())
+		.send({ title, price })
+		.expect(201);
+
+	expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
