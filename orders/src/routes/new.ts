c9aa8271a7880854077e689458ed73ce1542a8b0
Luka Maisuradze <lmaisuradz3@gmail.com>
2021-06-25 17:12:37 +0400

    Publish an event when creating orders

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -5,6 +5,8 @@ import { body } from "express-validator";
 
 import { Ticket } from "../models/ticket";
 import { Order, OrderStatus } from "../models/order";
+import { OrderCreatedPublisher } from "../events/publishers/order-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -45,6 +47,17 @@ router.post(
 		});
 
 		await order.save();
+
+		new OrderCreatedPublisher(natsWrapper.client).publish({
+			id: order.id,
+			status: order.status,
+			userId: order.userId,
+			expiresAt: order.expiresAt.toISOString(),
+			ticket: {
+				id: ticket.id,
+				price: ticket.price,
+			},
+		});
 
 		res.status(201).send(order);
 	},
